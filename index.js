7b1a055b1f5b46d5971f7e5bf7d2401719ad3673
priyanka <priyankaparale8@gmail.com>
2022-12-10 19:58:52 +0530

    js added

--- index.js
+++ index.js
@@ -1,61 +1,40 @@
-// async function getFetchData(){
-//     await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
-//     .then((response) => {
-//         response.json()})
-//     .then(data => console.log(resp0nse.data))
-//     .catch(error => console.log(error))
-// }
-// getFetchData();
+let data = [];
+
+async function getCryptoapi() {
+    const jsondata = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+
+    const jsdata = await jsondata.json();
+    data = jsdata;
+    // data = JSON.stringify(this.jsdata, null, " ")
+    // console.log("test test"+ JSON.stringify(this.data, null, " "))
+    console.log(data)
 
 
-// let api = fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false")
-// api.then((response) => {
-//     console.log(response.status)
-//     console.log(response.ok)
-//     return response.json()
-// }).then((value2) => {
-//     console.log(value2)
-// })
 
+    data.forEach((name) => {
+        // let image = ${name.image}
+        const price_change_24h = name.price_change_24h
 
-// async function getCryptoapi(){
-//     const jsondata = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+        const color = price_change_24h > 0 ? "green" : "red"
 
-//     const jsdata = await jsondata.json()
-//     console.log(jsdata)
-// }
-// getCryptoapi();
+        data += "<tr>"
 
-fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false" , {
-    method: "GET",
-    headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-    }
-}).then((response) => {
-    console.log(response.status)
-    console.log(response.ok)
-    const data = response.json()
-    if(response.ok){
-        let coinsData = data
+        data += `<td style="padding: 20px 5px 5px 5px; text-align: left"><img src="${name.image}" alt="coin" height="30px" style="margin-left: 50px"/> ${name.name} <td>`
 
-        if(coinsData.length > 0){
-            var cryptCoins =""
-        }
+        data += `<td style="text-align: left"> ${name.symbol.toUpperCase()} <td>`
+
+        data += `<td style="text-align: right"> $${name.current_price} <td>`
         
-        //for loop starts
-        coinsData.forEach((data) => {
-        cryptoCoins += "<tr>"
-        cryptoCoins += `<td> ${coin.symbol} <td>`;
-        cryptoCoins += `<td> ${coin.id} <td>`;
-        cryptoCoins += `<td> ${coin.symbol} <td>`;
-        cryptoCoins += `<td> ${coin.symbol} <td>`;
-        cryptoCoins += `<td> ${coin.symbol} <td>`;
-        cryptoCoins += `<td> ${coin.symbol} <td>`;
+        data += `<td style="text-align: right "> $${name.total_volume} <td>`;
+
+        data += `<td style="color: brown; text-align: right "> 
+        ${name.price_change_24h.toFixed(2)}% <td>`;
+
+        data += `<td style="text-align: right"> Mkt Cap:${name.market_cap} <td>`;"<tr>";
+        // console.log(name.name)
+        // console.log(name.id)
     })
-        
-    }
 
-}).then((value2) => {
-    console.log(value2)
-})
+    document.getElementById("data").innerHTML = data
+}
+getCryptoapi();
